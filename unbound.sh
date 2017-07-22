5589b8b0b0b46b1ff0b2e75e87e72b368e8c9a9c
Max Dymond <cmeister2@gmail.com>
2017-07-22 23:49:24 +0100

    Change unbound.sh so it installs to the codebase folder

--- unbound.sh
+++ unbound.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+ROOTDIR=$PWD
+UNBOUND_INSTALL=$ROOTDIR/unbound
+
+mkdir -p ${UNBOUND_INSTALL}
+
 pushd unbound-1.6.4
 
-./configure --with-pythonmodule && make && make install
+./configure --with-conf-file=${UNBOUND_INSTALL} --with-pythonmodule && make && make install
+
+popd
